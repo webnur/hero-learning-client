e1e9717786c8f0bd93b874a20957b7023f116b79
webnur <mdnuruli291@gmai.com>
2022-10-26 10:59:19 +0600

    basic login page design done alhamdulillah

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+    }
+
+
     return (
-        <div>
-            this is login page;
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <Link className="label-text-alt link link-hover">Forgot password?</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className='btn btn-primary' type="submit" value="Submit" />
+                            </div>
+                        </form>
+
+
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,20 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <div><h2>Hero Learning</h2></div>
-            <div className='navbar'>
-                <Link to='/'>home</Link>
-                <Link to='/courses'>Courses</Link>
-                <Link to='/blogs'>Blogs</Link>
-                <Link to='/login'>LogIn</Link> 
-                <Link to='/register'>Register</Link> 
-            </div>
+        <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link to className="btn btn-ghost normal-case text-xl">Hero Learning</Link>
         </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li> <Link to='/'>home</Link></li>
+            <li><Link to='/courses'>Courses</Link></li>
+            <li><Link to='/blogs'>Blogs</Link></li>
+            <li><Link to='/login'>LogIn</Link> </li>
+            <li> <Link to='/register'>Register</Link> </li>
+          </ul>
+        </div>
+      </div>
     );
 };
 
