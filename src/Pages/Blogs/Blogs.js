1a724ee5f88d484c00a10e09a0f9989ad6599e58
webnur <mdnuruli291@gmai.com>
2022-10-27 11:46:08 +0600

    blog page added alhamdulillah

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,49 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>This is blog page</h2>
+        <div className='lg:w-3/5 mx-auto mt-10'>
+            <div className="collapse mb-4">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <h2 className='text-xl'>what is cors?</h2>
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>
+                        Cross-origin resource sharing (CORS) is a browser security feature that restricts cross-origin HTTP requests that are initiated from scripts running in the browser. If your REST API's resources receive non-simple cross-origin HTTP requests, you need to enable CORS support.</p>
+                </div>
+            </div>
+
+            <div className="collapse mb-4">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <h2 className='text-xl'>Why are you using firebase? What other options do you have to implement authentication?</h2>
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.</p>
+                </div>
+            </div>
+
+            <div className="collapse mb-4">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <h2 className='text-xl'>How does the private route work?</h2>
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).
+                    </p>
+                </div>
+            </div>
+
+            <div className="collapse mb-4">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                <h2 className='text-xl'>What is Node? How does Node work?</h2>
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
+</p>
+                </div>
+            </div>
         </div>
     );
 };
