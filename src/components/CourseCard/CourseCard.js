a34df9d95a4846448cb86fd359586228185daad4
webnur <mdnuruli291@gmai.com>
2022-12-22 01:27:09 +0600

    card style changed

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -5,7 +5,7 @@ const CourseCard = ({ course }) => {
     // console.log(course)
     const { name, photo,duration, id} = course;
     return (
-        <div className="card card-compact w-80 bg-base-100 shadow-xl mb-4">
+        <div className="card card-compact bg-base-100 shadow-xl mb-4">
             <figure><img src={photo} style={{height: '200px'}} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">duration: {duration}</h2>
